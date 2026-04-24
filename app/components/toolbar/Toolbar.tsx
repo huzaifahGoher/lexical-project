eee17c40315b7b6684dd17314761227818754b05
Huzaifah Goher <huzaifah191001@gmail.com>
2026-04-24 18:12:55 +0500

    Used updated version of library and updated toolbar.tsx

--- app/components/toolbar/Toolbar.tsx
+++ app/components/toolbar/Toolbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 // import { Button } from "../button/Button";
 import { Button } from "@huzaifah191001/design-library";
@@ -6,13 +6,33 @@ import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext
 import { FORMAT_TEXT_COMMAND } from "lexical";
 
 const Toolbar = () => {
-    const [editor] = useLexicalComposerContext();
+  const [editor] = useLexicalComposerContext();
   return (
     <div className="max-h-10 flex flex-row flex-1 gap-2 items-center pl-2 border border-(--muted-foreground) rounded-sm bg-(--primary-foreground)">
-      <Button onClick={()=>{editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold")}} title="bold" children={<span className="text-gray-400">bold</span>}/>
-      <Button onClick={()=>{editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic")}} title="italic" children={<span className="text-gray-400">italic</span>}/>
-      <Button onClick={()=>{editor.dispatchCommand(FORMAT_TEXT_COMMAND, "underline")}} title="underline" children={<span className="text-gray-400">underline</span>}/>
-      <Button onClick={()=>{editor.dispatchCommand(FORMAT_TEXT_COMMAND, "strikethrough")}} title="strikethrough" children={<span className="text-gray-400">strikethrough</span>}/>
+      <Button
+        onClick={() => {
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold");
+        }}
+        children={<span>bold</span>}
+      />
+      <Button
+        onClick={() => {
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic");
+        }}
+        children={<span>italic</span>}
+      />
+      <Button
+        onClick={() => {
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "underline");
+        }}
+        children={<span>underline</span>}
+      />
+      <Button
+        onClick={() => {
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "strikethrough");
+        }}
+        children={<span>strikethrough</span>}
+      />
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+'use client'
 import Image from "next/image";
 import Editor from "./components/editor/Editor";
 import Toolbar from "./components/toolbar/Toolbar";
+import { ThemeProvider } from "@huzaifah191001/design-library";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 font-sans bg-(--background)">
-      <Editor />
-    </div>
+    <ThemeProvider>
+      <div className="flex flex-col flex-1 font-sans bg-(--background)">
+        <Editor />
+      </div>
+    </ThemeProvider>
   );
 }
