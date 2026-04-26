4b5910182be4a99645c183e068ae85a2b8391655
Huzaifah Goher <huzaifah191001@gmail.com>
2026-04-26 15:29:08 +0500

    Created utils file for Toolbar.tsx

--- app/components/toolbar/Toolbar.tsx
+++ app/components/toolbar/Toolbar.tsx
@@ -3,55 +3,17 @@ import React from "react";
 import { Button } from "@huzaifah191001/design-library";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import {
-  $createParagraphNode,
-  $getSelection,
   FORMAT_ELEMENT_COMMAND,
   FORMAT_TEXT_COMMAND,
-  REDO_COMMAND,
-  UNDO_COMMAND,
 } from "lexical";
-import {
-  INSERT_ORDERED_LIST_COMMAND,
-  INSERT_UNORDERED_LIST_COMMAND,
-} from "@lexical/list";
-import { $convertToMarkdownString } from "@lexical/markdown";
-import { $setBlocksType } from "@lexical/selection";
-import {
-  $createHeadingNode,
-  $createQuoteNode,
-  HeadingTagType,
-} from "@lexical/rich-text";
 import FloatingMenu from "../floatingmenu/FloatingMenu";
 import useKeyDownHandler from "./hooks/useKeyDownHandler";
 import { formattingOptions, alignmentOptions, listOptions, undoRedoOptions, blockOptions } from "./constants/ToolbarConstant";
-import { globalConstants } from "@/app/constants/global/GlobalConstants";
+import { exportMarkDown, getCarretPosition, handleHeading } from "./utils/ToolbarUtils";
 
 const Toolbar = () => {
   const [editor] = useLexicalComposerContext();
   const { showMenu, setShowMenu } = useKeyDownHandler();
-
-  const exportMarkDown = () => {
-    editor.update(() => {
-      const mdContent = $convertToMarkdownString(undefined, undefined);
-      const blob = new Blob([mdContent], { type: "text/markdown" });
-      const link = document.createElement("a");
-      link.href = URL.createObjectURL(blob);
-      link.download = "document.md";
-      link.click();
-    });
-  };
-
-  const handleHeading = (headingType = globalConstants.BLOCK.VALUES.H1) => {
-    editor.update(() => {
-      const selection = $getSelection();
-      if (!selection) return;
-      $setBlocksType(selection, () => {
-        if (headingType === globalConstants.BLOCK.VALUES.NORMAL) return $createParagraphNode();
-        if (headingType === globalConstants.BLOCK.VALUES.QUOTE) return $createQuoteNode();
-        return $createHeadingNode(headingType as HeadingTagType);
-      });
-    });
-  };
 
   return (
     <div className="max-h-10 flex flex-row flex-1 gap-2 items-center pl-2 border border-(--muted-foreground) rounded-sm bg-(--primary-foreground)">
@@ -91,10 +53,10 @@ const Toolbar = () => {
           children={<span>{item.label}</span>}
         />
       ))}
-      <Button onClick={exportMarkDown}>Export markdown</Button>
+      <Button onClick={()=>exportMarkDown(editor)}>Export markdown</Button>
       <select
         onChange={(event) => {
-          handleHeading(event.target.value);
+          handleHeading(editor, event.target.value);
         }}
       >
         {blockOptions.map((item: any, index) => (
@@ -108,6 +70,7 @@ const Toolbar = () => {
 
       {showMenu && (
         <FloatingMenu
+          position={getCarretPosition(showMenu)}
           onSelect={() => {
             setShowMenu(false);
           }}
---
+++ app/components/toolbar/utils/ToolbarUtils.ts
@@ -0,0 +1,42 @@
+import { globalConstants } from "@/app/constants/global/GlobalConstants";
+import { $convertToMarkdownString } from "@lexical/markdown";
+import { $createHeadingNode, $createQuoteNode, HeadingTagType } from "@lexical/rich-text";
+import { $setBlocksType } from "@lexical/selection";
+import { $createParagraphNode, $getSelection, LexicalEditor } from "lexical";
+
+
+const exportMarkDown = (editor: LexicalEditor) => {
+  editor.update(() => {
+    const mdContent = $convertToMarkdownString(undefined, undefined);
+    const blob = new Blob([mdContent], { type: "text/markdown" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "document.md";
+    link.click();
+  });
+};
+
+const getCarretPosition = (showMenu: boolean) => {
+  if (!showMenu) return undefined;
+  const selection = window.getSelection();
+  if (!selection || selection.rangeCount === 0) return undefined;
+  const range = selection.getRangeAt(0);
+  const rect = range.getBoundingClientRect();
+  return { top: rect.top, left: rect.left };
+};
+
+const handleHeading = (editor: LexicalEditor, headingType = globalConstants.BLOCK.VALUES.H1) => {
+  editor.update(() => {
+    const selection = $getSelection();
+    if (!selection) return;
+    $setBlocksType(selection, () => {
+      if (headingType === globalConstants.BLOCK.VALUES.NORMAL)
+        return $createParagraphNode();
+      if (headingType === globalConstants.BLOCK.VALUES.QUOTE)
+        return $createQuoteNode();
+      return $createHeadingNode(headingType as HeadingTagType);
+    });
+  });
+};
+
+export {handleHeading, getCarretPosition, exportMarkDown}
