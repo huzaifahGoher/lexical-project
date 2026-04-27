db3998255bc012d71947420b44281644d5482a48
Huzaifah Goher <huzaifah191001@gmail.com>
2026-04-27 20:11:13 +0500

    Added styles for ImageNodeDecorator.tsx.

--- app/customnodes/imagenode/ImageNodeDecorator.tsx
+++ app/customnodes/imagenode/ImageNodeDecorator.tsx
@@ -1,17 +1,14 @@
-import { LexicalEditor } from 'lexical'
 import React, { JSX } from 'react'
-import { ImageNode } from './imageNode';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { SerializedImageNodeType } from '../types/customNodeTypes';
+import "./ImageNodeDecorator.css";
 
 type ImageNodeDecoratorProps = {node: SerializedImageNodeType}
 
 export function ImageNodeDecorator({node} : ImageNodeDecoratorProps) : JSX.Element {
-  console.log(node)
   const [editor] = useLexicalComposerContext();
   return (
-    <div className='image-class inline-block'>
-      doobshakalaka
+    <div className='image-class inline-block' >
       <img src={node.src}/>
     </div>
   )
