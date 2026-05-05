853a7b7280c0e97bfda8b650cb8f25e14cfe3837
Huzaifah Goher <huzaifah191001@gmail.com>
2026-05-05 17:18:38 +0500

    Created a plugin for loading editor state if session storage has the json editorState.

---
+++ app/components/plugins/serializationplugin/SerializationPlugin.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+
+const SerializationPlugin = () => {
+  const [editor] = useLexicalComposerContext();
+  useEffect(() => {
+    const data = sessionStorage.getItem("data");
+    if (!data) return;
+    const editorstate = editor.parseEditorState(data);
+    if (!editorstate) return;
+    editor.update(()=>{
+        editor.setEditorState(editorstate);
+    })
+  }, []);
+  return null;
+};
+
+export default SerializationPlugin;
