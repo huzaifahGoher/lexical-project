59958d5555c14d855f357f0da2dbccc6fcb617a3
Huzaifah Goher <huzaifah191001@gmail.com>
2026-05-10 11:47:58 +0500

    Added control for theme in status bar.

--- app/components/plugins/statusbarplugin/StatusBarPlugin.tsx
+++ app/components/plugins/statusbarplugin/StatusBarPlugin.tsx
@@ -1,22 +1,30 @@
+import { useAppDispatch, useAppSelector } from "@/lib/hook";
+import { RootState } from "@/lib/store";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { $getRoot, EditorState } from "lexical";
+import Image from "next/image";
 import React, { useState } from "react";
 
 const StatusBarPlugin = () => {
-  const [editor] = useLexicalComposerContext();
-  const [data, setData] = useState({wordCount: 0, readingTime: 0, characterCount: 0})
+  const dispatch = useAppDispatch();
+  const theme = useAppSelector((state: RootState) => state.themeObject.theme);
+  const [data, setData] = useState({
+    wordCount: 0,
+    readingTime: 0,
+    characterCount: 0,
+  });
 
   const handleChange = (editorState: EditorState) => {
-    editorState.read(()=>{
-        const root = $getRoot();
-        const text = root.getTextContent();
-        const characterCount = root.getTextContentSize();
-        const wordCount = text.split(' ').length || 0;
-        const readingTime = wordCount*0.375;
+    editorState.read(() => {
+      const root = $getRoot();
+      const text = root.getTextContent();
+      const characterCount = root.getTextContentSize();
+      const wordCount = text.split(" ").length || 0;
+      const readingTime = wordCount * 0.375;
 
-        setData({wordCount, readingTime, characterCount});
-    })
+      setData({ wordCount, readingTime, characterCount });
+    });
   };
 
   return (
@@ -32,15 +40,29 @@ const StatusBarPlugin = () => {
         padding: "5px",
         display: "flex",
         flexFlow: "row wrap",
-        gap: "4px"
+        gap: "4px",
       }}
     >
-        <span>Word Count: </span>
-        <span>{data.wordCount}</span>
-        <span>Character Count: </span>
-        <span>{data.characterCount}</span>
-        <span>Reading Time: </span>
-        <span>{data.readingTime}</span>
+      <span>{theme} Theme: </span>
+      <Image
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          dispatch({
+            type: "theme/setTheme",
+            payload: `${theme === "light" ? "dark" : "light"}`,
+          });
+        }}
+        width={20}
+        height={20}
+        alt=""
+        src={theme === "light" ? "./light-theme.svg" : "./dark-theme.svg"}
+      />
+      <span>Word Count: </span>
+      <span>{data.wordCount}</span>
+      <span>Character Count: </span>
+      <span>{data.characterCount}</span>
+      <span>Reading Time: </span>
+      <span>{data.readingTime}</span>
       <OnChangePlugin onChange={handleChange} />
     </div>
   );
