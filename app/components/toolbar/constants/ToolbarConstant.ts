91cf9abd3541f8506f94a81212d9a763b71938d0
Huzaifah Goher <huzaifah191001@gmail.com>
2026-05-10 12:24:36 +0500

    Mapped svgs to constant variable.

--- app/components/toolbar/constants/ToolbarConstant.ts
+++ app/components/toolbar/constants/ToolbarConstant.ts
@@ -6,22 +6,27 @@ const formattingOptions = [
   {
     dispatchValue: globalConstants.FORMATTING.BOLD,
     label: globalConstants.FORMATTING.BOLD,
+    iconSrc: "./formatting/bold.svg"
   },
   {
     dispatchValue: globalConstants.FORMATTING.ITALIC,
     label: globalConstants.FORMATTING.ITALIC,
+    iconSrc: "./formatting/italic.svg",
   },
   {
     dispatchValue: globalConstants.FORMATTING.UNDERLINE,
     label: globalConstants.FORMATTING.UNDERLINE,
+    iconSrc: "./formatting/underline.svg",
   },
   {
     dispatchValue: globalConstants.FORMATTING.STRIKETHROUGH,
     label: globalConstants.FORMATTING.STRIKETHROUGH,
+    iconSrc: "./formatting/strikethrough.svg",
   },
   {
     dispatchValue: globalConstants.FORMATTING.CODE,
     label: globalConstants.FORMATTING.CODE,
+    iconSrc: "./formatting/code.svg",
   },
 ];
 
@@ -29,18 +34,22 @@ const alignmentOptions = [
   {
     dispatchValue: globalConstants.ALIGNMENT.LEFT,
     label: globalConstants.ALIGNMENT.LEFT,
+    iconSrc: "./alignment/left.svg",
   },
   {
     dispatchValue: globalConstants.ALIGNMENT.CENTER,
     label: globalConstants.ALIGNMENT.CENTER,
+    iconSrc: "./alignment/center.svg",
   },
   {
     dispatchValue: globalConstants.ALIGNMENT.RIGHT,
     label: globalConstants.ALIGNMENT.RIGHT,
+    iconSrc: "./alignment/right.svg",
   },
   {
     dispatchValue: globalConstants.ALIGNMENT.JUSTIFY,
     label: globalConstants.ALIGNMENT.JUSTIFY,
+    iconSrc: "./alignment/justify.svg",
   },
 ];
 
@@ -48,10 +57,12 @@ const listOptions = [
   {
     dispatchCommand: INSERT_ORDERED_LIST_COMMAND,
     label: globalConstants.LIST.LABELS.NUMBER,
+    iconSrc: "./list/ordered.svg",
   },
   {
     dispatchCommand: INSERT_UNORDERED_LIST_COMMAND,
     label: globalConstants.LIST.LABELS.BULLET,
+    iconSrc: "./list/unordered.svg",
   },
 ];
 
@@ -67,8 +78,8 @@ const blockOptions = [
 ];
 
 const undoRedoOptions = [
-  { label: globalConstants.UNDO_REDO.LABELS.UNDO, command: UNDO_COMMAND },
-  { label: globalConstants.UNDO_REDO.LABELS.REDO, command: REDO_COMMAND },
+  { label: globalConstants.UNDO_REDO.LABELS.UNDO, command: UNDO_COMMAND, iconSrc: "./history/undo.svg" },
+  { label: globalConstants.UNDO_REDO.LABELS.REDO, command: REDO_COMMAND, iconSrc: "./history/redo.svg" },
 ];
 
 export {formattingOptions, alignmentOptions, listOptions, blockOptions, undoRedoOptions};
