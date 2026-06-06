19755904a7c9a604b5dff05839f4385b8f3f830b
Huzaifah Goher <huzaifah191001@gmail.com>
2026-06-06 10:56:31 +0500

    removing unused imports.

--- app/components/editor/Editor.tsx
+++ app/components/editor/Editor.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
