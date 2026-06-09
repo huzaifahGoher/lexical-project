5b34ca654ac5a47bb0a5418ae9d4a06704800f15
Huzaifah Goher <huzaifah191001@gmail.com>
2026-06-10 01:52:08 +0500

    Imported CustomNodes in editor.tsx

--- app/components/editor/Editor.tsx
+++ app/components/editor/Editor.tsx
@@ -36,8 +36,7 @@ import { debounce } from "lodash";
 import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
 import { TablePlugin } from "@lexical/react/LexicalTablePlugin";
 import {
-  CustomTableCellNode, CustomTableNode, CustomTableRowNode,
-} from "@/app/customnodes/tablenode/tableNode";
+  CustomTableCellNode, CustomTableNode, CustomTableRowNode } from "@/app/customnodes/tablenode/tableNode";
 import { $createCustomTableCellNode, $createCustomTableNode, $createCustomTableRowNode } from "@/app/customnodes/utils/customNodeUtils";
 
 const config = {
