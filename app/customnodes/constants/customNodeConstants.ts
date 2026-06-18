8efe533cdde61a032ecb2f76835feade2feab0f2
Huzaifah Goher <huzaifah191001@gmail.com>
2026-06-18 10:08:35 +0500

    Updated constants.

--- app/customnodes/constants/customNodeConstants.ts
+++ app/customnodes/constants/customNodeConstants.ts
@@ -1,7 +1,7 @@
 export const customNodesConstants = {
     IMAGE: {TYPE: "imageNode"},
     TABLE: {TYPE: "tableNode",
-        ROW_TYPE: "tableRowNode",
+    ROW_TYPE: "tableRowNode",
         CELL_TYPE: "tableCellNode"
     },
     LINK: {
