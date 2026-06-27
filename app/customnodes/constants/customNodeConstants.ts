1d5cfe4f7323624cea550e3dbc15b0bf072f4359
Huzaifah Goher <huzaifah191001@gmail.com>
2026-06-27 11:39:35 +0500

    Updated custom nodes type value.

--- app/customnodes/constants/customNodeConstants.ts
+++ app/customnodes/constants/customNodeConstants.ts
@@ -2,7 +2,8 @@ export const customNodesConstants = {
     IMAGE: {TYPE: "imageNode"},
     TABLE: {TYPE: "tableNode",
     ROW_TYPE: "tableRowNode",
-    CELL_TYPE: "tableCellNode"},
+    CELL_TYPE: "tableCellNode"}
+    ,
     LINK: {
         TYPE: "customLinkNode"
     }
